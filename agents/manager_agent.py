d068e9ac843da233de712016c8b527ced938b992
Willy <thewillyeb@gmail.com>
2025-03-12 23:16:23 +0000

    minor changes

--- agents/manager_agent.py
+++ agents/manager_agent.py
@@ -5,7 +5,15 @@ from models import openai_model
 manager_agent = CodeAgent(
     tools=[],
     model=openai_model,
-    managed_agents=[jira_issues_agent]
+    managed_agents=[jira_issues_agent],
+    additional_authorized_imports=[
+        "geopandas",
+        "plotly",
+        "shapely",
+        "json",
+        "pandas",
+        "numpy",
+    ],
 )
 
 def execute_multiagent(user_input):
